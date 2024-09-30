07b4c2e9322f7cd73ac6b9101fd366daf7f6ce58
Freddy Trae Torres <ftorres0448@gmail.com>
2024-09-30 10:54:02 -0500

    Added DB Connection and get request

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const sql = require("mssql");
 
 require('dotenv').config();
 
 const PORT = process.env.PORT;
+const password = process.env.PASSWORD;
+const config = {
+    user: 'freddytrae',
+    password: password,
+    server: 'traeserver.database.windows.net',
+    port: 1433,
+    database: 'CheckBookAPI',
+    authentication: {
+        type: 'default'
+    },
+    options: {
+        encrypt: true
+    }
+}
 
-app.get("/api", (req, res) => {
-    res.json({fruits: ["banana", "orange", "watermelon", "avocado"]});
+app.get("/getUserBalance", async (req, res) => {
+    try {
+        var poolConnection = await sql.connect(config);
+        console.log("connection worked");
+        var resultSet = await poolConnection.request().query('SELECT UserAndBalance.UserBalance FROM UserAndBalance WHERE UserAndBalance.UserID=1');
+        var userBalance
+        resultSet.recordset.forEach(row => {
+            userBalance = row.UserBalance;
+        })
+        res.json({"User's Balance": `${userBalance}`});
+    } catch(err) {
+        console.log(err.message);
+    }
 })
 
 app.listen(PORT, () => {
