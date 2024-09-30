443455c5bd4fd332b0e71b370794baa2de16b6b8
Freddy Trae Torres <ftorres0448@gmail.com>
2024-09-29 20:48:16 -0500

    initial commit

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+require('dotenv').config();
+
+const PORT = process.env.PORT;
 
 app.get("/api", (req, res) => {
-    res.json({fruits: ["apple", "orange", "watermelon"]});
+    res.json({fruits: ["banana", "orange", "watermelon"]});
 })
 
-app.listen(8080, () => {
-    console.log("Server started on port 8080");
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 })
